2879af29943aadf7f873cd61d306c3d95e82085c
Saud <qahmad@chicagobooth.edu>
2017-08-16 17:38:46 +0000

    Changes to photo controller to require login for all pages

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
 
-skip_before_action :authenticate_user!, :only => [:index, :show]
+# skip_before_action :authenticate_user!, :only => [:index, :show]
 
   def favorites
     @photos = current_user.liked_photos
